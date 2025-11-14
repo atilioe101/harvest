8b25948aaa452f537805c7d66ca8fd0f4270568b
aerrecaborde <aerrecaborde@southernlights.com.ar>
2025-11-14 14:01:20 -0300

    add firebase sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -53,6 +54,8 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.ui)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(platform(libs.firebase.bom))
+    implementation (libs.firebase.analytics)
 
 
 
